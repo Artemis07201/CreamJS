6f6707a8c086d75efa858050da4bce7c66401c21
Artemis07201 <artemis07201@gmail.com>
2022-08-14 13:04:31 -0500

    penis

--- commands/hug.js
+++ commands/hug.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder} = require('discord.js')
+const {SlashCommandBuilder, EmbedBuilder, Embed} = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('hug')
@@ -9,17 +9,23 @@ module.exports = {
         .setTitle('Hug')
         .setDescription('*Hugs <@' + interaction.options.getUser('user').id + '>*')
         .setColor('ffbff0')
-        var image = data.read('./gifs/hugs.json', 'hugs')
-        embed.setImage(image[Math.floor(Math.random()*image.length)])
-        if(!data.exists(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount')){
-            data.write(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount', 1)
-            embed.setFooter({text:interaction.options.getUser('user').username + ' has been hugged 1 time'})
+        if(interaction.user.id === interaction.options.getUser('user').id){
+            var reply = data.read('./responses/hug_self.json', 'hug_self')
+            embed.setDescription(reply[Math.floor(Math.random()*reply.length)] + ' <@' + interaction.options.getUser('user').id + ">")
+            await interaction.reply({embeds:[embed]})
         }else{
-            var hugged = parseInt(data.read(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount'))
-            hugged += 1
-            data.write(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount', hugged)
-            embed.setFooter({text:interaction.options.getUser('user').username + ` has been hugged ${hugged} times`})
+            var image = data.read('./responses/hugs.json', 'hugs')
+            embed.setImage(image[Math.floor(Math.random()*image.length)])    
+            if(!data.exists(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount')){
+                data.write(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount', 1)
+                embed.setFooter({text:interaction.options.getUser('user').username + ' has been hugged 1 time'})
+            }else{
+                var hugged = parseInt(data.read(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount'))
+                hugged += 1
+                data.write(`./users/${interaction.options.getUser('user').id}.json`, 'hug_amount', hugged)
+                embed.setFooter({text:interaction.options.getUser('user').username + ` has been hugged ${hugged} times`})
+            }
+            await interaction.reply({embeds:[embed]})
         }
-        await interaction.reply({embeds:[embed]})
     }
 }
